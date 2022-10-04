235dfcdf348addf25c7c8e94d761e4efa69ba045
AdityaApte145 <adityaapte2014@gmail.com>
2022-10-04 23:41:49 +0530

    Updated Triangle_Types.cpp

--- C++/Triangle_Types.cpp
+++ C++/Triangle_Types.cpp
@@ -2,7 +2,7 @@
 #include <conio.h>
 using namespace std;
 
-void main()
+int main()
 {
     int i, j, k, choice;
 
@@ -21,145 +21,158 @@ void main()
 
     switch (choice)
     {
-    case 1:
-        for (i = 1; i <= 5; i++)
-        {
-            for (j = 1; j <= i; j++)
+        case 1:{
+            for (i = 1; i <= 5; i++)
             {
-                cout << "*";
-            }
-            cout << endl;
-        }
-        break;
-
-    case 2:
-        for (i = 1; i <= 5; i++)
-        {
-            for (j = 1; j <= i; j++)
-            {
-                cout << j;
-            }
-            cout << endl;
-        }
-        break;
-
-    case 3:
-        for (i = 1; i <= 5; i++)
-        {
-            for (j = 1; j <= i; j++)
-            {
-                cout << ((char)(i + 64));
-            }
-            cout << endl;
-        }
-        break;
-    case 4:
-        for (i = 5; i >= 1; i--)
-        {
-            for (j = 1; j <= i; j++)
-            {
-                cout << "*";
-            }
-            cout << endl;
-        }
-        break;
-
-    case 5:
-        for (i = 5; i >= 1; i--)
-        {
-            for (j = 1; j <= i; j++)
-            {
-                cout << j;
-            }
-            cout << endl;
-        }
-        break;
-
-    case 6:
-        for (i = 1; i <= 5; i++)
-        {
-            for (j = i; j < 5; j++)
-            {
-                cout << " ";
-            }
-            for (k = 1; k < (i * 2); k++)
-            {
-                cout << "*";
-            }
-            cout << endl;
-        }
-        break;
-
-    case 7:
-        for (i = 1; i <= 5; i++)
-        {
-            for (j = i; j < 5; j++)
-            {
-                cout << " ";
-            }
-            for (k = 1; k < (i * 2); k++)
-            {
-                cout << k;
-            }
-            cout << endl;
-        }
-        break;
-
-    case 8:
-        for (i = 5; i >= 1; i--)
-        {
-            for (j = 5; j > i; j--)
-            {
-                cout << " ";
-            }
-            for (k = 1; k < (i * 2); k++)
-            {
-                cout << "*";
-            }
-            cout << endl;
-        }
-        break;
-    case 9:
-        int c = 1;
-
-        for (i = 0; i < 6; i++)
-        {
-            for (j = 1; j <= 6 - 1; j++)
-            {
-                cout << " ";
-            }
-            for (k = 0; k <= i; k++)
-            {
-                if (k == 0 || i == 0)
+                for (j = 1; j <= i; j++)
                 {
-                    c = 1;
+                    cout << "*";
                 }
-                else
-                {
-                    c = c * (i - k + 1) / k;
-                }
-                cout << c << " ";
+                cout << endl;
             }
-            cout << endl;
+            break;
         }
-        break;
 
-    case 10:
-        int num = 1;
-
-        for (i = 1; i <= 5; i++)
-        {
-            for (j = 1; j <= i; ++j)
+        case 2:{
+            for (i = 1; i <= 5; i++)
             {
-                cout << num++ << " ";
+                for (j = 1; j <= i; j++)
+                {
+                    cout << j;
+                }
+                cout << endl;
             }
-            cout << endl;
+            break;
         }
-        break;
 
-    default:
-        cout << "INVALID CHOICE";
-        break;
+        case 3:{
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    cout << ((char)(i + 64));
+                }
+                cout << endl;
+            }
+            break;
+        }
+
+        case 4:{
+            for (i = 5; i >= 1; i--)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    cout << "*";
+                }
+                cout << endl;
+            }
+            break;
+        }
+
+        case 5:{
+            for (i = 5; i >= 1; i--)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    cout << j;
+                }
+                cout << endl;
+            }
+            break;
+        }
+
+        case 6:{
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = i; j < 5; j++)
+                {
+                    cout << " ";
+                }
+                for (k = 1; k < (i * 2); k++)
+                {
+                    cout << "*";
+                }
+                cout << endl;
+            }
+            break;
+        }
+
+        case 7:{
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = i; j < 5; j++)
+                {
+                    cout << " ";
+                }
+                for (k = 1; k < (i * 2); k++)
+                {
+                    cout << k;
+                }
+                cout << endl;
+            }
+            break;
+        }
+
+        case 8:{
+            for (i = 5; i >= 1; i--)
+            {
+                for (j = 5; j > i; j--)
+                {
+                    cout << " ";
+                }
+                for (k = 1; k < (i * 2); k++)
+                {
+                    cout << "*";
+                }
+                cout << endl;
+            }
+            break;
+        }
+
+        case 9:{
+            int c = 1;
+
+            for (i = 0; i < 6; i++)
+            {
+                for (j = 1; j <= 6 - 1; j++)
+                {
+                    cout << " ";
+                }
+                for (k = 0; k <= i; k++)
+                {
+                    if (k == 0 || i == 0)
+                    {
+                        c = 1;
+                    }
+                    else
+                    {
+                        c = c * (i - k + 1) / k;
+                    }
+                    cout << c << " ";
+                }
+                cout << endl;
+            }
+            break;
+        }
+
+        case 10:{
+            int num = 1;
+
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = 1; j <= i; ++j)
+                {
+                    cout << num++ << " ";
+                }
+                cout << endl;
+            }
+            break;
+        }
+
+        default:{
+            cout << "INVALID CHOICE";
+            break;
+        }
     }
 
     getch();
