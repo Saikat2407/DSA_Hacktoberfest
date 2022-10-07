08a1edd8ff0e41ce3ea12aa8a4fbae103b4b9e05
deepshikha2708 <80972038+deepshikha2708@users.noreply.github.com>
2022-10-07 22:17:03 +0530

    Update quick_sort.py

--- Python/sorting_algrithms/quick_sort.py
+++ Python/sorting_algrithms/quick_sort.py
@@ -1,3 +1,14 @@
+# The Quick Sort Algorithm initially requires selecting a pivot element, based on which the array division will be implemented. This pivot element selection can be carried out in four ways:
+
+#To pick the first element of the array
+#To pick the last element of the array 
+#To pick a random element from the array
+#Here we implement the algorithm using the last element as the pivot in each of the sub-arrays.
+
+#Then the array in question is partitioned into two halves by considering each element to be greater or lesser than the pivot element. Elements that are lesser than the pivot element are put on the left-hand side of the pivot, and the ones greater are put on the right. The pivot element is ultimately placed in the center.
+
+#Thus all the elements on the left are smaller than the pivot element, and all the elements on the right are bigger. The Quick Sort algorithm is then applied on each of the sub-arrays created after division, individually.
+
 def partition(array, left, right, pivot):
     while left < right:
         while left < right:
@@ -28,3 +39,5 @@ def QuickSort(array, start, stop):
 array1 = [8, 9, 2, 0, 7, 1, 6, 4, 3, 5]
 QuickSort(array1, 0, len(array1)-1)
 print(array1)
+
+# Time Complexity: O(n^2) 
