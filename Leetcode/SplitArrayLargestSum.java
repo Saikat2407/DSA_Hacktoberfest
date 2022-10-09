b9581a8e0ea70b2b880d2fa5153edad2ac1c0f0c
Shourya Khanka <shouryakhanka@gmail.com>
2022-10-09 21:55:29 +0530

    Added Split array largest sum code.

---
+++ Leetcode/SplitArrayLargestSum.java
@@ -0,0 +1,46 @@
+/* 410. Split Array Largest Sum
+
+Given an integer array nums and an integer k, split nums into k non-empty subarrays such that the largest sum of any subarray is minimized.
+
+Return the minimized largest sum of the split.
+
+A subarray is a contiguous part of the array.
+
+*/
+class Solution {
+    public int splitArray(int[] nums, int m) {
+      int start = 0;
+    int end = 0;
+    for (int i = 0; i < nums.length; i++) { // in the end, the loop will contain the maximum item from the array
+        start = Math.max(start, nums[i]);
+        end += nums[i];
+    }
+
+    // binary search 
+    while (start < end) {
+        // try for the middle as the potential ans
+        int mid = start + (end - start) / 2;
+        // calculate how many pieces you can divide this in with the max sum
+        int sum = 0;
+        int pieces = 1;
+        for (int num : nums) {
+            if (sum + num > mid) {
+                // you cannot add this in this subarray, make a new one
+                // say you add this num in this new subarray, then sum = num
+                sum = num;
+                pieces++;
+            }
+            else {
+                sum += num;
+            }
+        }
+        if (pieces > m) {
+            start = mid + 1;
+        }
+        else {
+            end = mid;
+        }
+    }
+        return end;
+    }  
+    }
