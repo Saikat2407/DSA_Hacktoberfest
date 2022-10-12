57e63c8996e4015db12d402b1f5bebe33c9ea932
Abhishek Kanaujiya <65384193+abhi26ak@users.noreply.github.com>
2022-10-12 22:56:53 +0530

    Create Container_with_most_water.cpp

---
+++ C++/Container_with_most_water.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxArea(vector<int>& h) {
+        int ans = 0;
+        int i = 0 , j = h.size() - 1;
+        while(i < j)
+        {
+            ans = max(ans , min(h[i] , h[j])*(j-i));
+            if(h[i] > h[j])
+                j--;
+            else
+                i++;
+        }
+        return ans;
+    }
+};
