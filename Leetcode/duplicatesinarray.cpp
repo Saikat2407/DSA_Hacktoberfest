89046cd8643a2d83665573831afae19095a0616e
Abul Aman <abul2002aman@gmail.com>
2022-10-16 21:08:11 +0530

    duplicatesinarray.cpp

--- Leetcode/duplicatesinarray.cpp
+++ Leetcode/duplicatesinarray.cpp
@@ -1,31 +1,18 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-vector<int> duplicatesInArray(int arr[], int n){
-    vector<int>ans;
-    for(int i=0;i<n;i++){
-        int key=arr[i]%n;
-        arr[key]+=n;
-    }
-    for(int i=0;i<n;i++){
-        if((arr[i]/n)>=2){
-            ans.push_back(i);
+https://leetcode.com/problems/find-all-duplicates-in-an-array/
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int>ans;
+        for(int n:nums){
+            n=abs(n);
+            if(nums[n-1]>0){
+                nums[n-1] *=-1;
+            }
+            else{
+                ans.push_back(n);
+            }
         }
+        return ans;
     }
-    if(ans.size()==0){
-        return {-1};
-    }
-    else return ans;
-}
+};
 
-
-int main(){
-    int arr[]={4,2,1,0,1,2,6};
-    int n=sizeof(arr)/sizeof(arr[0]);
-    vector<int>res=duplicatesInArray(arr,n);
-    cout<<"The repeating elements are: "<<endl;
-    for(int i:res){
-        cout<< i <<" "<<endl;
-    } 
-    return 0;
-    }
