6f160b235f32cee0cd34d1b753a1f7b2bace8623
Himalaya Prakash <54323654+Himalaya001@users.noreply.github.com>
2022-10-24 11:52:41 +0530

    Create DIVCLASS.java

---
+++ Leetcode/DIVCLASS.java
@@ -0,0 +1,37 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class MyCode
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scan=new Scanner(System.in);
+		int T=scan.nextInt();
+		while(T-->0)
+		{
+		    int N=scan.nextInt();
+		    int a[]=new int[N];
+		    HashSet<Integer> hs=new HashSet<Integer>();
+    		    for(int i=0;i<N;i++)
+    		    {
+    		        a[i]=scan.nextInt();
+    		        hs.add(a[i]);
+    		    }
+		    if(N<12)
+		    {
+		        System.out.println("no");
+		    }
+		    else
+		    {
+		        if(hs.size()>12)
+		            System.out.println("no");
+		        else
+		            System.out.println("yes");
+		    }
+		}
+	}
+}
